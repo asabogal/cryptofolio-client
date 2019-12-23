e08b5a0ea7fbfbf561d7a446ec69698f201f69c1
asabogal <asabogal.git@gmail.com>
2019-12-23 18:38:57 -0500

    fix loggedIn props

--- src/components/hoc/Layout.js
+++ src/components/hoc/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../header_footer/Header'
 
-const Layout = ({handleLogin, handleLogout, loggedIn, children}) => {
+const Layout = ({handleLogout, loggedIn, children}) => {
   return (
     <div>
       <Header
